2135d0896d85779e58c2737926a7f7323565848c
sueyan9 <409133742@qq.com>
2025-05-31 00:04:55 +1200

    test:loginForm

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy"
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+    testEnvironment: 'jsdom',
+};
