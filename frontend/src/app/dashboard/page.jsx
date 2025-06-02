0fba5ca5d26e592b2e8b6363e46b24be447f060e
sueyan9 <409133742@qq.com>
2025-06-02 12:48:05 +1200

    fix: add React  page.js

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { React,useState } from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import "./Dashboard.css";
